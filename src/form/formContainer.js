e47b96cc3ed471b1d03f2aeb2cc643417c0183c9
US\amidash <amidash@deloitte.com>
2018-07-31 10:59:41 +0530

    First commit.

---
+++ src/form/formContainer.js
@@ -0,0 +1,21 @@
+import { withStateHandlers } from 'recompose';
+import { connect } from 'react-redux';
+import { textAction, submitAction } from './actions';
+
+const mapStateToProps = state => state.form;
+const mapDispatchToProps = { textAction, submitAction };
+
+const FormContainer = withStateHandlers(
+    ({ text = '' }) => ({ text }),
+    {
+        onTextChange: () => event => ({
+            text: event.target.value,
+        }),
+        onFormSubmit: ({ text }) => (event) => {
+            event.preventDefault();
+            alert(text);
+        },
+    },
+);
+
+export default connect(mapStateToProps, mapDispatchToProps);
